eff448c0cd630ad99c7acb2183ce4231031a4ce0
liorcole <75811780+liorcole@users.noreply.github.com>
2023-10-15 18:49:08 -0400

    Update scrape_imgmodels.py

--- scrape_imgmodels.py
+++ scrape_imgmodels.py
@@ -11,26 +11,26 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 # Find the HTML elements containing the model information
-model_elements = soup.find_all("div", class_="model__details")
+model_elements = soup.find_all("a", class_="listItemcomponent__StyledListItem-sc-31qhyr-2")
 
 # Initialize a list to store the model data
 models_data = []
 
 # Loop through the model elements and extract data
 for model_element in model_elements:
-    name = model_element.find("div", class_="model__name").text.strip()
-    height_element = model_element.find("div", class_="model__height")
-    height = height_element.find("div", class_="number").text.strip()
-    followers_element = model_element.find("div", class_="model__instaFollowers")
-    followers = int(followers_element.find("div", class_="number").text.replace(",", ""))
+    name = model_element.get("title")
+    image = model_element.find("img")["src"]
+    height_element = model_element.find("span", text="Height")
+    height = int(height_element.find_next("p").text.split('-')[0].strip()[:-2])
     
     # Create the model data dictionary
     model_data = {
         "name": name,
-        "height": int(height),
-        "hair_color": "_",  # You can add this data if it's available on the site
-        "image": f"images/{name.lower().replace(' ', '')}.png",
-        "followers": followers
+        "height": height,
+        "hair_color": "_",
+        "image": image,
+        "followers": 0,
+        # You can add other properties here
     }
     
     # Append the model data to the list

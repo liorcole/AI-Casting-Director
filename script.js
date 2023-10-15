2f9205f038d06cb7d85bab682c6b009e10e9a2e4
liorcole <75811780+liorcole@users.noreply.github.com>
2023-10-15 14:00:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
+// Define your data
+const modelsData = [
+    {
+        name: 'Abby Bush',
+        height: 180,
+        hair_color: 'Blonde',
+        image: 'public/abbybush.png', // Image URL for Abby Bush
+        // Add other properties as needed
+    },
+    {
+        name: 'Aalyah Ross',
+        height: 179,
+        hair_color: 'Black',
+        image: 'public/aalyahross.png', // Image URL for Aalyah Ross
+        // Add other properties as needed
+    },
+    // {
+    //     name: 'Test',
+    //     height: 180,
+    //     hair_color: 'Blonde',
+    //     image: 'test.png', // Image URL for Test
+    //     // Add other properties as needed
+    // },
+];
+
+// Event listener for form submission
 document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const hairColor = document.getElementById('hair-color').value;
     const height = document.getElementById('height').value;
-    const url = `/models?hair_color=${hairColor}&height=${height}`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const resultsDiv = document.getElementById('results');
-            resultsDiv.innerHTML = ''; // Clear previous results
-            data.forEach(model => {
-                const modelImage = document.createElement('img');
-                modelImage.src = model.image_url;
-                modelImage.alt = 'Model Image';
+    // Your search logic using the modelsData array
+    const filteredModels = modelsData.filter(model => {
+        return model.hair_color.toLowerCase() === hairColor.toLowerCase() &&
+            model.height === parseInt(height);
+    });
 
-                const modelInfo = document.createElement('p');
-                modelInfo.innerHTML = `Name: ${model.name}<br>Age: ${model.age}<br>Height: ${model.height}<br>Hair Color: ${model.hair_color}`;
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ''; // Clear previous results
 
-                const modelCard = document.createElement('div');
-                modelCard.appendChild(modelImage);
-                modelCard.appendChild(modelInfo);
+    filteredModels.forEach(model => {
+        const modelCard = document.createElement('div');
 
-                resultsDiv.appendChild(modelCard);
-            });
-        })
-        .catch(error => console.error('Error:', error));
+        // Create and append an image with the custom URL
+        const modelImage = document.createElement('img');
+        modelImage.src = model.image; // Use the image URL from the model's data
+        modelCard.appendChild(modelImage);
+
+        // Create and append model information
+        const modelInfo = document.createElement('p');
+        modelInfo.innerHTML = `Name: ${model.name}<br>Height: ${model.height}<br>Hair Color: ${model.hair_color}`;
+        modelCard.appendChild(modelInfo);
+
+        resultsDiv.appendChild(modelCard);
+    });
 });

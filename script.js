fe3c929bf8073d3be90e805f6061f8f14e417020
liorcole <75811780+liorcole@users.noreply.github.com>
2023-10-17 13:39:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ document.getElementById('search-form').addEventListener('submit', function (e) {
     const hairColors = Array.from(document.querySelectorAll('input[name="hair_color"]:checked')).map(input => input.value);
     const minHeightCm = parseFloat(document.getElementById('min-height-cm').value) || 0; // Set a default of 0
     const maxHeightCm = parseFloat(document.getElementById('max-height-cm').value) || 300; // Set a default of 300 cm
-    const hairType = document.getElementById('hair-type').value.toLowerCase();
+    const selectedHairTypes = Array.from(document.querySelectorAll('input[name="hair_type"]:checked')).map(input => input.value);
     const sizeType = document.getElementById('size-type').value.toLowerCase();
     const instagramFollowers = parseInt(document.getElementById('instagram-followers').value);
 
@@ -268,7 +268,7 @@ document.getElementById('search-form').addEventListener('submit', function (e) {
         return (hairColors.length === 0 || hairColors.some(color => model.hair_color.includes(color))) &&
             (model.height >= minHeightCm) && // No need to check if minHeightCm is 0
             (model.height <= maxHeightCm) && // No need to check if maxHeightCm is 300
-            (hairType === "" || model.hair_type.includes(hairType)) &&
+            (selectedHairTypes.length === 0 || selectedHairTypes.some(type => model.hair_type.includes(type))) &&
             (sizeType === "" || model.type.includes(sizeType)) &&
             (model.instagram >= instagramFollowers);
     });

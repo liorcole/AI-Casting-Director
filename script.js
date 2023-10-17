31c1c5af25f9971788215a217359636a3d5a468f
liorcole <75811780+liorcole@users.noreply.github.com>
2023-10-17 02:58:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -151,16 +151,6 @@ document.getElementById('search-form').addEventListener('submit', function (e) {
     const sizeType = document.getElementById('size-type').value.toLowerCase();
     const instagramFollowers = parseInt(document.getElementById('instagram-followers').value);
 
-   // Get the values from the height sliders
-    const minHeightFeet = parseInt(document.getElementById('min-height-feet').value); // CHANGED HERE
-    const minHeightInches = parseInt(document.getElementById('min-height-inches').value); // CHANGED HERE
-    const maxHeightFeet = parseInt(document.getElementById('max-height-feet').value); // CHANGED HERE
-    const maxHeightInches = parseInt(document.getElementById('max-height-inches').value);
-
-    / Convert height values to centimeters
-    const minHeightCm = (minHeightFeet * 30.48) + (minHeightInches * 2.54); // CHANGED HERE
-    const maxHeightCm = (maxHeightFeet * 30.48) + (maxHeightInches * 2.54); // CHANGED HERE
-
     // Your search logic using the modelsData array
     const filteredModels = modelsData.filter(model => {
         return (hairColor === "" || model.hair_color.some(color => color.toLowerCase() === hairColor)) &&
